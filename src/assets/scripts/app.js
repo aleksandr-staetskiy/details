ded45b96ccbd526ba3a3ca343e3e036d43a0d019
aleksandr-staetskiy <aleksandrstaetskiy@gmail.com>
2019-10-08 13:30:30 +0300

    amount btns logic

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -44,3 +44,30 @@ function orderedAccordion() {
 }
 
 orderedAccordion();
+
+
+function minusAmount() {
+    const btnMinus = document.querySelectorAll('.field-num__btn-minus');
+
+    btnMinus.forEach( function(item) {
+        item.addEventListener( 'click', function() {
+         const chosenBtn = this.parentElement.querySelector('.field-num__input');
+         let minus = chosenBtn.value--;
+        })
+    })
+}
+
+minusAmount();
+
+function plusAmount() {
+    const btnMinus = document.querySelectorAll('.field-num__btn-plus');
+
+    btnMinus.forEach( function(item) {
+        item.addEventListener( 'click', function() {
+         const chosenBtn = this.parentElement.querySelector('.field-num__input');
+         let plus = chosenBtn.value++;
+        })
+    })
+}
+
+plusAmount();
