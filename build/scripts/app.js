3d5998b19a8428b7680a20d304125513c0e6e20b
aleksandr-staetskiy <aleksandrstaetskiy@gmail.com>
2019-10-11 12:47:07 +0300

    search-dropdown js

--- build/scripts/app.js
+++ build/scripts/app.js
@@ -9878,22 +9878,29 @@ jquery__WEBPACK_IMPORTED_MODULE_0___default()(document).ready(function () {
 const input = document.querySelector('.search__input ');
 const searchResult = document.querySelector('.search-dropdown');
 const btn = document.querySelector('.btn');
-const search = document.querySelector('.search');
+const search = document.querySelector('.page-header__search');
 input.addEventListener('focus', function (e) {
   if (e.target === input) {
-    searchResult.style.display = "block";
+    showPageSearch();
     window.addEventListener('click', function (e) {
-      if (!e.target.closest('.search')) {
+      if (!e.target.closest('.page-header__search')) {
         searchResult.style.display = "none";
-      }
-    });
-    window.removeEventListener('click', function (e) {
-      if (!e.target.closest('.search')) {
-        searchResult.style.display = "none";
+        window.removeEventListener('click', hidePageSearch(e));
       }
     });
   }
 });
+
+function hidePageSearch(e) {
+  if (!e.target.closest('.page-header__search')) {
+    searchResult.style.display = "none";
+  }
+}
+
+function showPageSearch() {
+  searchResult.style.display = "block";
+}
+
 console.log('it works bitch');
 
 /***/ })
